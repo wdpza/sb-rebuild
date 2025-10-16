e8c8265fdf9bf1e3a33b8e1707b6f131e848fb72
Clinton <clinton@starbright.co.za>
2025-10-16 09:14:35 +0000

    Latest GrapQl query

--- src/lib/graphql/queries/getPageBySlug.ts
+++ src/lib/graphql/queries/getPageBySlug.ts
@@ -5,73 +5,149 @@ export const GET_PAGE_BY_SLUG = gql`
         page(id: $slug, idType: URI) {
             title
             pageFieldGroup {
-            pageBuilder {
-                __typename
-                ... on PageFieldGroupPageBuilderHeroLayout {
-                title
-                description
-                subTitle
-                background {
-                    node {
-                    mediaItemUrl
-                    mediaDetails {
-                        file
-                    }
-                    }
-                }
-                image {
-                    node {
-                    mediaItemUrl
-                    }
-                }
-                }
-                ... on PageFieldGroupPageBuilderServicesAccordionLayout {
-                content {
+                pageBuilder {
+                    __typename
+                    ... on PageFieldGroupPageBuilderHeroLayout {
                     title
                     description
-                }
-                }
-                ... on PageFieldGroupPageBuilderLogoSliderLayout {
-                logo {
+                    subTitle
+                    background {
+                        node {
+                        mediaItemUrl
+                        mediaDetails {
+                            file
+                        }
+                        }
+                    }
+                    image {
+                        node {
+                        mediaItemUrl
+                        }
+                    }
+                    }
+                    ... on PageFieldGroupPageBuilderServicesAccordionLayout {
+                    content {
+                        title
+                        description
+                    }
+                    }
+                    ... on PageFieldGroupPageBuilderLogoSliderLayout {
                     logo {
-                    node {
+                        logo {
+                        node {
+                            altText
+                            mediaItemUrl
+                            sizes
+                            sourceUrl
+                            srcSet
+                        }
+                        }
+                    }
+                    }
+                    ... on PageFieldGroupPageBuilderPortfolioSectionLayout {
+                    numberOfItems
+                    title
+                    }
+                    ... on PageFieldGroupPageBuilderBusinessSolutionsLayout {
+                    numberOfItems
+                    title
+                    }
+                    ... on PageFieldGroupPageBuilderStatsSectionLayout {
+                    statsFirst
+                    statsSecond
+                    statsThird
+                    }
+                    ... on PageFieldGroupPageBuilderExitSectionLayout {
+                    title
+                    backgroundImage {
+                        node {
                         altText
                         mediaItemUrl
-                        sizes
-                        sourceUrl
-                        srcSet
+                        mediaDetails {
+                            file
+                        }
+                        }
+                    }
+                    link {
+                        target
+                        title
+                        url
                     }
                     }
-                }
-                }
-                ... on PageFieldGroupPageBuilderPortfolioSectionLayout {
-                numberOfItems
-                title
-                }
-                ... on PageFieldGroupPageBuilderBusinessSolutionsLayout {
-                numberOfItems
-                title
-                }
-                ... on PageFieldGroupPageBuilderStatsSectionLayout {
-                statsFirst
-                statsSecond
-                statsThird
-                }
-                ... on PageFieldGroupPageBuilderExitSectionLayout {
-                title
-                backgroundImage {
-                    node {
-                    altText
-                    mediaItemUrl
-                    }
-                }
-                link {
-                    target
+                    ... on PageFieldGroupPageBuilderTeamsSectionLayout {
+                    description
+                    subTitle
                     title
-                    url
+                    }
+                    ... on PageFieldGroupPageBuilderCertificationSectionLayout {
+                    fieldGroupName
+                    certificates {
+                        description
+                        logo {
+                        node {
+                            altText
+                            mediaItemUrl
+                        }
+                        }
+                        title
+                    }
+                    }
+                    ... on PageFieldGroupPageBuilderEmployeeCarouselLayout {
+                    description
+                    employeePhotos {
+                        nodes {
+                        altText
+                        mediaItemUrl
+                        }
+                    }
+                    title
+                    }
+                    ... on PageFieldGroupPageBuilderLeftGallerySectionLayout {
+                    description
+                    fieldGroupName
+                    gallery {
+                        nodes {
+                        altText
+                        mediaItemUrl
+                        }
+                    }
+                    title
+                    }
+                    ... on PageFieldGroupPageBuilderRightImageSectionLayout {
+                    description
+                    fieldGroupName
+                    image {
+                        node {
+                        altText
+                        mediaItemUrl
+                        }
+                    }
+                    title
+                    }
+                    ... on PageFieldGroupPageBuilderPortfolioTabsLayout {
+                    perCategory
+                    categories {
+                        nodes {
+                        termTaxonomyId
+                        slug
+                        name
+                        databaseId
+                        }
+                    }
+                    }
+                    ... on PageFieldGroupPageBuilderBlogTabsLayout {
+                    fieldGroupName
+                    perPage
+                    categories {
+                        nodes {
+                        databaseId
+                        name
+                        slug
+                        termTaxonomyId
+                        }
+                    }
+                    }
                 }
-                }
-            }
             }
         }
     }
