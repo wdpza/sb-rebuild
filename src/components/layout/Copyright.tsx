9a95519f4913c437f28b0ebc66d6f6613f70f780
Willem <willem@starbright.co.za>
2025-10-29 06:03:26 +0000

    Safely handle null menu items in footer copyright

--- src/components/layout/Copyright.tsx
+++ src/components/layout/Copyright.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 
-export default function Copyright( footer: any) {
+export default function Copyright({ footer }: { footer?: any }) {
 
     const currentYear = new Date().getFullYear();
+    const menuNodes = footer?.footer?.policiesMenu?.menuItems?.nodes ?? [];
 
     return (
         <div className="bg-[#49474D] w-full">
             <div className="max-w-[1600px] mx-auto p-4 flex justify-between">
                 <div className="text-white">Starbright © 2005 - {currentYear}  - All Rights Reserved.</div>
                 <div className="flex space-x-3 text-neutral-600">
-                    {footer.footer.policiesMenu.menuItems.nodes.map((item: any, index: number) => (
-                        <React.Fragment key={item.id}>
-                        <a href={item.url} className="text-white">
-                            {item.label}
+                    {menuNodes.map((item: any, index: number) => (
+                        <React.Fragment key={item?.id ?? index}>
+                        <a href={item?.url ?? '#'} className="text-white">
+                            {item?.label ?? ''}
                         </a>
-                        {index < footer.footer.policiesMenu.menuItems.nodes.length - 1 && (
+                        {index < menuNodes.length - 1 && (
                             <span className="text-white">|</span>
                         )}
                         </React.Fragment>
