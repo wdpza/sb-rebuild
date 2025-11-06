6d1a2ccc50fea85833866da89b64b8a157ab52f6
Willem <willem@starbright.co.za>
2025-11-06 11:23:58 +0000

    Portpolio loading skeleton added

---
+++ src/app/portfolio/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-6 px-8">
+        <div className='min-h-[300px] animate-pulse p-4 pt-8 bg-muted rounded bg-[#49474D]'  ></div>
+        <div className='min-h-[300px] animate-pulse p-4 pt-8 bg-muted rounded bg-[#49474D]'  ></div>
+        <div className='min-h-[300px] animate-pulse p-4 pt-8 bg-muted rounded bg-[#49474D]'  ></div>
+    </div>
+  );
+} 
