114137e1f0d0c5341319a5d53dfe574f71bd6353
Wikus du Plessis <wikus@starbright.co.za>
2026-08-18 13:38:36 +0200

    Added Facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,34 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         </noscript>
         {/* End Google Tag Manager (noscript) */}
 
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '840942659370318');
+          fbq('track', 'PageView');
+          fbq('track', 'CompleteRegistration');
+          fbq('track', 'Lead');
+          fbq('track', 'SubmitApplication');
+          fbq('track', 'ViewContent');`}
+        </Script>
+        <noscript>
+          <img
+            alt=""
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=840942659370318&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
         <Header menu={menu} logo={logo} />
         {children}
         <Footer footer={footer} />
